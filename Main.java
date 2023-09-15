b4b9feaf30f784a5027479adc567cdfa002dd14e
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-09-15 16:26:26 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int familyKids = 4;
+        boolean isItRaining = false;
+        double gasPrice = 3.59;
+        int favNumber = 8;
+        double shoeSize = 10.0;
+        String birthMonth = "May";
+        String name = "Ben Fahrmeier";
+    }
+}
